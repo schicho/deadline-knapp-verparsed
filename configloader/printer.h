975b55c82b98baa9d5657ac78c4e4e18b98d200f
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2026-03-26 16:16:37 +0100

    add comment

--- configloader/printer.h
+++ configloader/printer.h
@@ -4,12 +4,20 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/*
+These functions wrap the stdio functions to create another level of indirection.
+Instead of printing using the stdio.h functions, use these.
+*/
+
+/* print to stdout */
 int cfg_printf(const char* format, ...);
 int cfg_vprintf(const char* format, va_list args);
 
+/* print to stderr */
 int cfg_err_printf(const char* format, ...);
 int cfg_err_vprintf(const char* format, va_list args);
 
+/* print to any file */
 int cfg_fprintf(FILE* stream, const char* format, ...);
 int cfg_vfprintf(FILE* stream, const char* format, va_list args);
 
