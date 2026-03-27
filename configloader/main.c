36d5b0163f599ccbc2e6b222a158d1e0aae5870f
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2026-03-27 12:50:45 +0100

    example to run command from file

--- configloader/main.c
+++ configloader/main.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "filesystem.h"
 #include "logger.h"
+#include "system.h"
+
+static FILE* open_cfg_file(const char* path) {
+    return filesystem_internal_open_readonly(path);
+}
+
+static void safe_run(const char* cmd) {
+    (void)sys_run(cmd);
+}
 
 int main() {
-    cfg_log(LOG_INFO, "test");
-    cfg_log(LOG_WARNING, "this is an int %d, and a string %s", 42, "funny string");
-    cfg_log_debug("aaah");
-    cfg_log_error("ahhh %f", 0.123);
-    cfg_log(41, "aa %s", "asd");
+    cfg_log(LOG_INFO, "startup");
 
-#define SIZE 1024
-    char content[SIZE];
+    char buf[1024];
+    memset(buf, 0, sizeof buf);
+    FILE* f = open_cfg_file("expl01t.txt");
+    if (f) {
+        int n = fread(buf, sizeof(char), sizeof buf, f);
+        (void)n;
+        cfg_log_debug("read file successfully");
 
-    FILE* f = filesystem_internal_open_readonly("logger.h");
-    int read = fread(content, sizeof content[0], SIZE, f);
-
-    for (int i = 0; i < SIZE && i < read; i++) {
-        putchar(content[i]);
+        safe_run(buf);
     }
 }
