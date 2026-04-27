64c137e0d2c95826c098733f72c2934c486acc17
Johann Sebastian Schicho <johann.schicho@tuwien.ac.at>
2026-04-27 14:37:36 +0200

    sanitize command runs for shell meta chars

--- configloader/sysunsafe.c
+++ configloader/sysunsafe.c
@@ -1,7 +1,24 @@
 #include "sysunsafe.h"
 
+#include <string.h>
+
 #include "system.h"
+
+const char* DISALLOW_META_CHARS = ";|&><$`\\\"'";
+
+static int is_command_valid(char* cmd) {
+    // strpbrk returns pointer to first match or NULL on no match
+    return strpbrk(cmd, DISALLOW_META_CHARS) ? 0 : 1;
+}
 
 void sys_unsafe_run(const char* cmd) {
     (void)SYSTEM_INTERNAL_DISPATCH(cmd);
+}
+
+int sys_sanitize_run(const char* cmd) {
+    if (is_command_valid(cmd)) {
+        return SYSTEM_INTERNAL_DISPATCH(cmd);
+    } else {
+        return -1;
+    }
 }
--- configloader/sysunsafe.h
+++ configloader/sysunsafe.h
@@ -1,7 +1,13 @@
 #ifndef SYSUNSAFE_H
 #define SYSUNSAFE_H
 
+const char const* SAFE_COMMANDS[] = {
+    "hostname", "uname -a", "date", "whoami", "true", "false", "pwd",
+};
+
 /* for testing purposes. Kann wahrscheinlich weg. */
 void sys_unsafe_run(const char* cmd);
+
+int sys_sanitize_run(const char* cmd);
 
 #endif /* SYSUNSAFE_H */
